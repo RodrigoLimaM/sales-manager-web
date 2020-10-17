3704ce8e5a6bcb59d4b7f8facb10c0472eb79642
RodrigoLimaM <digo100200@gmail.com>
2020-10-17 17:53:10 -0300

    initializing

---
+++ src/main/java/br/com/salesmanagerweb/SalesManagerWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.salesmanagerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagerWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/salesmanagerweb/SalesManagerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.salesmanagerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
